52e9a07bc916690f3bec170ef91e7f34f8ba2b4a
andrew knackstedt <knackstedt@outlook.com>
2024-10-14 17:16:07 -0400

    feat: read json file for configuration

--- src/environment.ts
+++ src/environment.ts
@@ -1,8 +1,18 @@
-export const environment = {
-    is_production: false,
-    port: 3000,
+import fs from 'fs';
+import json5 from 'json5';
 
-    projects: [
+let conf = '{}';
+try { conf = fs.readFileSync('/data/config.json', 'utf8') } catch(ex) {}
+
+const configuration = json5.parse(conf);
+
+const is_production = process.env['NODE_ENV'] == 'production';
+
+export const environment = {
+    is_production,
+    port: configuration.port || 3000,
+
+    projects: configuration.projects || [
         {
             domain: "localhost",
             path: "/mnt/volume1/websites/example.com.crystalproj",
